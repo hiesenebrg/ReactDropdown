0b28270e2624c85da4316d7ae5d2f47f5e286ad5
hiesenebrg <adarshsingh8008@gmail.com>
2023-02-16 06:19:41 +0530

    first and final commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Dropdown from "./Dropdown";
+import styled from "styled-components";
+function App() {
+
+  return (
+    < Container>
+          <Dropdown />
+</Container>
+
+  );
+}
+const Container = styled.div`
+height: 50vh;
+width:40vw;
+background-color: Orange;
+margin:auto;
+`
+
+export default App;
